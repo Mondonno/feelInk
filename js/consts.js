22fd013c03142774d1c7ba4314ca8ad87f2b86c9
Mondonno <mikolaj.mocek@icloud.com>
2021-10-16 21:47:59 +0200

    feat: Consitency consts updates

--- js/consts.js
+++ js/consts.js
@@ -1,6 +1,6 @@
 let speedsConsts = {
     mouseNormal: 0.8,
-    touchpadMacOSConsistency: 155,
+    touchpadMacOSConsistency: 180, // 155
     touchpadMacOS: 0.25,
     touchpadMacOSUser: 0.17,
     touchpadMacOSPrecis: 0.15
